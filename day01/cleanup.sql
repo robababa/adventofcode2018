d7147d98cdefa1af6c9ea3a13c94f49f133e14da
Rob Johnson <robj@hightouchinc.com>
2018-12-02 14:04:15 -0600

    Add cleanup sql files

---
+++ day01/cleanup.sql
@@ -0,0 +1,2 @@
+drop function if exists f_day01_part2 cascade;
+drop table if exists day01, day01_part2 cascade;
---
+++ day02/cleanup.sql
@@ -0,0 +1,2 @@
+drop function if exists f_day02_part2 cascade;
+drop table if exists day02, day02_split, day02_part2, day02_part2_diffs cascade;
