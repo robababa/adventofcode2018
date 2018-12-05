4eadb04539c772f27bb08011ba14bf9e1c513e81
Rob Johnson <robj@hightouchinc.com>
2018-12-05 09:58:05 -0600

    Day 5 part 1
    Takes a long time - 24 seconds just to get the input how I want it, and
    8.5 minutes to solve.

--- day05/part1.sql
+++ day05/part1.sql
@@ -6,18 +6,24 @@ create table day05 (full_string text);
 
 \copy day05 from './input.txt';
 
-create table day05_chars (id serial not null, ch char(1) not null, previous_ch char(1) not null);
+create table day05_chars (
+  id serial not null,
+  ch char(1) not null,
+  previous_id bigint null,
+  previous_ch char(1) null
+);
 
 -- break out the characters into individual rows
 -- this takes about 24 seconds on my machine
 do
 $$
   declare
+    my_id bigint := null;
     ch char(1) := ' ';
     previous_ch char(1) := ' ';
   begin
     foreach ch in array (select regexp_split_to_array(full_string, '') from day05 limit 1) loop
-      insert into day05_chars (ch, previous_ch) values (ch, previous_ch);
+      insert into day05_chars (ch, previous_id, previous_ch) values (ch, my_id, previous_ch) returning id into my_id;
       previous_ch := ch;
     end loop;
   end;
@@ -25,67 +31,53 @@ $$
 ;
 
 with update_source as (
-  select id, ch, coalesce(lag(ch) over (order by id), ' ') as previous_ch from day05_chars
+  select id, ch, lag(id) over (order by id) as previous_id, lag(ch) over (order by id) as previous_ch from day05_chars
 )
 update day05_chars
-set previous_ch = update_source.previous_ch
+set
+  previous_id = update_source.previous_id,
+  previous_ch = update_source.previous_ch
 from update_source
 where
 day05_chars.id = update_source.id;
 
-create index day05_chars_matches_idx on day05_chars (id, ch, previous_ch);
+create index day05_chars_matches_idx on day05_chars (id, ch, previous_id, previous_ch)
+where
+ch != previous_ch and upper(ch) = upper(previous_ch) and lower(ch) = lower(previous_ch);
 
--- break out the characters into individual rows
--- do
--- $$
---   declare
---     my_string_length int := 0;
---     my_string text := '';
---   begin
---     my_string_length := (select length(full_string) from day05);
---     my_string := (select full_string from day05);
---   for i in 1..length(my_string) loop
---     insert into day05_chars (ch, previous_ch) values (substr(my_string, i, 1)::char(1), ' '::char(1));
---   end loop;
---   end;
--- $$
--- ;
+create view first_reaction as
+select id, previous_id, ch, previous_ch
+from day05_chars
+where
+ch != previous_ch and upper(ch) = upper(previous_ch) and lower(ch) = lower(previous_ch)
+order by id
+limit 1;
 
--- create view first_reaction as
--- with possible_reactions as (
---   select
---    id,
---    ch,
---    lag(id) over (order by id) as previous_id,
---    lag(ch) over (order by id) as previous_ch
---   from day05_chars
---   order by id
--- )
--- select
--- id,
--- previous_id
--- from possible_reactions
--- where
--- upper(ch) = upper(previous_ch) and
--- lower(ch) = lower(previous_ch) and
--- ch != previous_ch
--- order by id
--- limit 1;
---
--- do
--- $$
---   declare
---     affected_rows int := -1;
--- begin
---     while (affected_rows != 0) loop
---       delete from day05_chars
---       using first_reaction
---       where
---       day05_chars.id = first_reaction.id or day05_chars.id = first_reaction.previous_id;
---       get diagnostics affected_rows := ROW_COUNT;
---     end loop;
---   end;
--- $$
--- ;
---
--- select count(*) as part1_answer from day05_chars;
+do
+$$
+  declare
+    reaction first_reaction%rowtype;
+    affected_rows int := 0;
+begin
+    loop
+      select * into reaction from first_reaction;
+      get diagnostics affected_rows := ROW_COUNT;
+      exit when affected_rows = 0;
+
+      -- update the row directly below the reaction, so its previous columns match the row directly above the reaction
+      update day05_chars
+      set
+      (previous_id, previous_ch) =
+        (select previous_id, previous_ch from day05_chars where id = reaction.previous_id)
+      where
+      previous_id = reaction.id;
+
+      -- now delete the rows in the reaction
+      delete from day05_chars where id = reaction.id or id = reaction.previous_id;
+
+    end loop;
+  end;
+$$
+;
+
+select count(*) as part1_answer from day05_chars;
