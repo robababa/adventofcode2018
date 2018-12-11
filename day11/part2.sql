9a4c639afe712782705b62c492cdd5588537b266
Rob Johnson <robj@hightouchinc.com>
2018-12-11 08:49:53 -0600

    Day 11 part 2 runs in 3 minutes, but is incorrect

--- day11/part2.sql
+++ day11/part2.sql
@@ -1,35 +1,35 @@
+-- 82,240,24 is wrong
+
 with recursive fuel_cell(x, y, size, value)
 as
   (
-    select x, y, 1 as size, value from day11
+    select x, y, 1 as size, value::bigint
+    from day11
     union all
-    (
-      select
-        fuel_cell.x - 1                         as x,
-        fuel_cell.y - 1                         as y,
-        fuel_cell.size + 1                      as size,
-        fuel_cell.value + sum(day11.value) as value
-      from
-        fuel_cell,
-        day11
-      where
-        -- the fuel cell isn't already hitting the boundary
-        fuel_cell.x > 1
-        and
-        fuel_cell.y > 1
-        and
-        -- we add coordinates directly left and above the current cell
+    select
+      fuel_cell.x - 1                   as x,
+      fuel_cell.y - 1                   as y,
+      fuel_cell.size + 1                as size,
+      fuel_cell.value + neighbors.value as value
+    from
+      fuel_cell,
+        lateral
         (
-            (day11.x = fuel_cell.x and day11.y = fuel_cell.y - 1) or
-            (day11.x = fuel_cell.x - 1 and day11.y = fuel_cell.y) or
-            (day11.x = fuel_cell.x - 1 and day11.y = fuel_cell.y - 1)
-          )
-      group by
-        fuel_cell.x - 1,
-        fuel_cell.y - 1,
-        fuel_cell.size + 1,
-        fuel_cell.value
-    )
+          select sum(day11.value) as value
+            from day11 where
+            (
+              (day11.x = fuel_cell.x and day11.y = fuel_cell.y - 1) or
+              (day11.x = fuel_cell.x - 1 and day11.y = fuel_cell.y) or
+              (day11.x = fuel_cell.x - 1 and day11.y = fuel_cell.y - 1)
+            )
+        )
+      as
+      neighbors
+    where
+        -- the fuel cell isn't already hitting the boundary
+      fuel_cell.x > 1
+      and
+      fuel_cell.y > 1
   )
 select
 x, y, size, value
