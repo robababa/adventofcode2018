95473f3680b7209d97f1628fb9ad775321904d49
Rob Johnson <robj@hightouchinc.com>
2018-12-14 12:20:35 -0600

    Day 13 part 1

--- day13/part1.sql
+++ day13/part1.sql
@@ -111,8 +111,10 @@ $$
     spots := day13_spots();
     loop
       for cart in (select * from day13_cart order by x, y) loop
+        /*
         raise notice 'moving cart % from x = %, y = % with direction % and next_turn %',
           cart.id, cart.x, cart.y, cart.direction, cart.next_turn;
+        */
         perform day13_move_cart(cart);
       end loop;
     end loop;
